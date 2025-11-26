9dc917d939f1e57b3822a08fb66d3cf09b22c29d
aghaeeyan <a.aghaeeyan@gmail.com>
2025-11-25 19:12:05 -0500

    Autocommit

--- lsFig.R
+++ lsFig.R
@@ -2,13 +2,17 @@ library(shellpipes)
 library(ggplot2); theme_set(theme_bw())
 library(patchwork)
 
-startGraphics(height=4)
+startGraphics(height=6)
 
 dens <- rdsRead("Density")
 curve <- rdsRead("Curves")
  
-print(dens + 
-	(curve + theme(legend.position = "none"))
+print(dens + 	curve
+	 # + theme(legend.position = "none")
+	 + plot_layout(
+	 ncol   = 2,         
+	 widths = c(1, 1)    
+)
 )
 
 
